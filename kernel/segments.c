608bb0215c50e711a85a52d9a3c1745f2cce3507
Eric Zhang <ezhang887@gmail.com>
2022-05-29 18:19:50 -0700

    Add segment creation

--- kernel/segments.c
+++ kernel/segments.c
@@ -1,14 +1,35 @@
 #include "segments.h"
 
-static gdt_entry_t gdt_entries[3];
+static void create_gdt_entry(gdt_entry_t *dst, int privilege_level, int type);
 
 void segments_init() {
+    gdt_entry_t gdt_entries[3];
     gdt_t gdt;
     gdt.address = (unsigned int) &gdt_entries;
     gdt.size = sizeof(gdt_entries);
 
-    // TODO: create gdt entries (NULL, kernel text, kernel data)
+    // NULL entry
+    create_gdt_entry(gdt_entries + 0, 0, 0);
+
+    // Kernel text segment
+    create_gdt_entry(gdt_entries + 1, 0, 0xA);
+
+    // Kernel data segment
+    create_gdt_entry(gdt_entries + 2, 0, 0x2);
 
     load_segment_selectors();
     load_gdt(&gdt);
+}
+
+static void create_gdt_entry(gdt_entry_t *dst, int privilege_level, int type) {
+    dst->base_low = 0x0;
+    dst->base_mid = 0x0;
+    dst->base_high = 0x0;
+
+    dst->limit_low = 0xFFFF;
+    dst->limit_high = 0xF;
+
+    dst->flags = (0x01 << 3) | (0x01 << 2);
+
+    dst->access = (0x1 << 7) | ((privilege_level & 0x3) << 5) | (0x1 << 4) | (type & 0xF);
 }
--- kernel/segments.h
+++ kernel/segments.h
@@ -10,11 +10,11 @@ typedef struct gdt_entry {
     // See https://wiki.osdev.org/Global_Descriptor_Table#Segment_Descriptor for layout
     unsigned short limit_low;
     unsigned short base_low;
-    unsigned char base_midh;
+    unsigned char base_mid;
     unsigned char access;
     unsigned char limit_high: 4;
     unsigned char flags: 4;
-    unsigned char base;
+    unsigned char base_high;
 } __attribute__((packed)) gdt_entry_t;
 
 
